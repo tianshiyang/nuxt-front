5cbc572ee3bce6835a6dcb228e70e176e618060d
田世洋 <tianshiyang@icourt.cc>
2023-08-01 21:45:38 +0800

    feat: 环境区分

---
+++ composables/request.ts
@@ -0,0 +1,71 @@
+import { merge } from 'lodash'
+type FetchType = typeof $fetch
+type ReqType = Parameters<FetchType>[0]
+type FetchOptions = Parameters<FetchType>[1]
+import { ElMessage } from 'element-plus'
+export function httpRequest<T = unknown>(
+  method: 'get' | "GET" | 'post' | "POST",
+  url: ReqType,
+  body?: any,
+  opts?: FetchOptions,
+) {
+  const token = useCookie('token')
+  const router = useRouter()
+  const route = useRoute()
+
+  let baseUrl = ""
+  if (process.env.NODE_ENV === 'development') {
+    baseUrl = 'http://127.0.0.1:7001'
+  }
+  url = baseUrl + url
+
+  const defaultOpts = {
+    method,
+    // baseURL: '/api',
+    headers: { token: token.value } as any,
+    body,
+    onRequestError() {
+      ElMessage.error('请求出错，请重试！')
+    },
+    onResponseError({ response }) {
+      switch (response.status) {
+        case 400:
+          ElMessage.error('参数错误')
+          break
+        case 401:
+          ElMessage.error('没有访问权限')
+          router.push(`/login?callback=${route.path}`)
+          break
+        case 403:
+          ElMessage.error('服务器拒绝访问')
+          break
+        case 404:
+          ElMessage.error('请求地址错误')
+          break
+        case 500:
+          ElMessage.error('服务器故障')
+          break
+        default:
+          ElMessage.error('网络连接故障')
+          break
+      }
+    },
+  } as FetchOptions
+
+  return $fetch<T>(url, merge(defaultOpts, opts))
+}
+
+export function httpPost<T = unknown>(
+  request: ReqType,
+  body?: any,
+  opts?: FetchOptions,
+) {
+  return httpRequest<T>('post', request, body, opts)
+}
+
+export function httpGet<T = unknown>(
+  request: ReqType,
+  opts?: FetchOptions,
+) {
+  return httpRequest<T>('get', request, null, opts)
+}
--- pages/Login/index.vue
+++ pages/Login/index.vue
@@ -6,26 +6,47 @@
     </section>
     <el-alert title="测试账号和密码都是test" type="success" show-icon />
 
-    <el-form style="margin-top: 20px;">
-      <el-form-item label="账号">
+    <el-form ref="loginRef" :model="form" :rules="rules" style="margin-top: 20px;">
+      <el-form-item label="账号" prop="username">
         <el-input v-model="form.username" placeholder="请输入用户名" clearable />
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input type="password" v-model="form.password" placeholder="请输入密码" clearable />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">登录</el-button>
+        <el-button type="primary" @click="handleLogin">登录</el-button>
       </el-form-item>
     </el-form>
   </el-card>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from "vue";
+import type { FormInstance, FormRules } from 'element-plus'
+import { reactive, ref } from "vue";
 const form = reactive({
   username: '',
   password: ''
 })
+const loginRef = ref<FormInstance>()
+const rules = reactive<FormRules<Record<string, any>>>({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+  ],
+})
+
+// 登录
+const handleLogin = async () => {
+  if (!loginRef.value) return
+  await loginRef.value.validate(async (valid) => {
+    if (valid) {
+      const result = await httpPost("/api/user/login", form)
+      console.log(result)
+    }
+  })
+}
 </script>
 
 <style scoped>
