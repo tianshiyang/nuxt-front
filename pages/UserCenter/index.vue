05475307dc15ea8e4d8ed5d444a6b2ee76f7ccf8
田世洋 <tianshiyang@icourt.cc>
2023-08-03 00:00:16 +0800

    feat: 更新密码

--- pages/UserCenter/index.vue
+++ pages/UserCenter/index.vue
@@ -28,18 +28,38 @@
       </el-form-item>
     </el-form>
   </el-card>
-  <el-card title="账号信息"></el-card>
+
+  <el-card style="margin-top: 20px;">
+    <template #header>
+      <h1>更改密码</h1>
+    </template>
+    <el-form label-width="120px" :model="passwordForm" ref="passwordFormRef" :rules="rules">
+      <el-form-item prop="oldPassword" label="请输入旧密码">
+        <el-input type="password" v-model="passwordForm.oldPassword" clearable placeholder="请输入旧密码" />
+      </el-form-item>
+      <el-form-item prop="newPassword" label="请输入新密码">
+        <el-input type="password" v-model="passwordForm.newPassword" clearable placeholder="请输入新密码" />
+      </el-form-item>
+      <el-form-item prop="rePassword" label="请确认新密码">
+        <el-input type="password" v-model="passwordForm.rePassword" clearable placeholder="请确认新密码" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleResetPassword">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script lang="ts" setup>
-import { useHead } from "nuxt/app";
+import { useCookie, useHead } from "nuxt/app";
 import { useUserInfo } from "@/store/user"
 import { storeToRefs } from "pinia";
 import { cloneDeep } from "lodash";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { httpPost, httpGet } from "@/composables/request";
 import { ElMessage } from "element-plus";
 import moment from "moment";
+import type { FormInstance } from 'element-plus'
 
 useHead({
   title: "用户中心"
@@ -70,6 +90,54 @@ const getUserInfo = async () => {
     newUserInfo.userInfo = result.data
   } catch(err) {
     ElMessage.error(err as string)
+  }
+}
+
+const passwordFormRef = ref<FormInstance>()
+
+const passwordForm = reactive({
+  oldPassword: "",
+  newPassword: "",
+  rePassword: ""
+})
+
+const rules = ref({
+  oldPassword: [
+    { required: true, message: '请输入旧密码', target: "blur" }
+  ],
+  newPassword: [
+    { required: true, message: '请输入新密码', target: "blur" }
+  ],
+  rePassword: [
+    { required: true, message: '请确认新密码', target: "blur" }
+  ]
+})
+
+const handleResetPassword = async () => {
+  await passwordFormRef.value?.validate((valid) => {
+    if (valid) {
+      if (passwordForm.newPassword !== passwordForm.rePassword) {
+        ElMessage.error('两次输入不一致')
+        return
+      }
+      handleUpdatePassword()
+    }
+  })
+}
+
+const handleUpdatePassword = async () => {
+  let result: any = null
+  try {
+    result = await httpPost("/api/user/updateUserPassword", passwordForm)
+  } catch(err) {
+    ElMessage.error(err)
+    return
+  }
+  ElMessage.success('更新密码成功')
+  passwordFormRef.value?.resetFields()
+  const token = useCookie('token')
+  if (result) {
+    token.value = result.data.token
   }
 }
 </script>
