9f1f766fed1b5bab19c1b2142bbf20abfab22d3b
田世洋 <tianshiyang@icourt.cc>
2023-08-06 11:14:04 +0800

    feat: 创建课程弹窗

---
+++ components/course/updateCourse.vue
@@ -0,0 +1,147 @@
+<template>
+  <el-dialog v-model="form.visible" :title="props.useType === 0 ? '新增课程' : '创建课程'">
+    <el-form :model="form" :rules="rules" :inline="false" ref="CreateCourseForm">
+      <el-form-item prop="title" label="课程标题">
+        <el-input v-model="form.title" placeholder="请输入课程标题" clearable />
+      </el-form-item>
+      <el-form-item prop="cover" label="课程封面">
+        <el-input v-model="form.cover" placeholder="请输入课程封面" clearable />
+      </el-form-item>
+      <el-form-item prop="price" label="课程原价">
+      <el-input v-model="form.price" placeholder="请输入课程原价" clearable />
+      </el-form-item>
+      <el-form-item prop="tPrice" label="课程现价">
+        <el-input v-model="form.tPrice" placeholder="请输入课程现价" clearable />
+      </el-form-item>
+      <el-form-item prop="desc" label="课程描述">
+        <el-input v-model="form.desc" placeholder="请输入课程描述" clearable />
+      </el-form-item>
+      <el-form-item prop="detail" label="课程简介">
+        <el-input v-model="form.detail" type="textarea" placeholder="请输入课程简介" clearable />
+      </el-form-item>
+      <el-form-item label="是否推荐">
+        <el-select v-model="form.isRecommend" placeholder="请选择是否推荐" clearable>
+          <el-option label="否" :value="0" />
+          <el-option label="是" :value="1" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="是否在栏目">
+        <el-select v-model="form.isColumn" placeholder="请选择是否在栏目" clearable>
+          <el-option label="否" :value="0" />
+          <el-option label="是" :value="1" />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleClick">确定</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue"
+import type { FormInstance } from 'element-plus'
+
+const props = defineProps({
+  useType: {
+    type: Number as PropType<0 | 1>, // 0：创建，1编辑
+    default: 0
+  },
+  courseId: Number
+})
+
+const CreateCourseForm = ref<FormInstance>()
+
+const form = reactive({
+  visible: true,
+  courseId: "",
+  title: "",
+  cover: "",
+  price: "",
+  tPrice: "",
+  isRecommend: "",
+  isColumn: "",
+  desc: "",
+  detail: ""
+})
+
+const rules = {
+  title: [
+    { required: true, message: '请输入课程标题', trigger: 'blur' },
+  ],
+  price: [
+    { required: true, message: '请输入课程原价', trigger: 'blur' },
+  ],
+  tPrice: [
+    { required: true, message: '请输入课程现价', trigger: 'blur' },
+  ],
+  desc: [
+    { required: true, message: '请输入课程描述', trigger: 'blur' },
+  ],
+  detail: [
+    { required: true, message: '请输入课程简介', trigger: 'blur' },
+  ],
+}
+
+const getUpdateCourseDetail = async () => {
+  let result = null
+  try {
+    await httpGet("/api/course/createCourse", {
+      params: {
+        form 
+      }
+    })
+  } catch (err) {
+    ElMessage.error(err as any)
+    return
+  }
+  form.title = (result as any).detail.title
+  form.cover = (result as any).detail.cover
+  form.price = (result as any).detail.price
+  form.tPrice = (result as any).detail.tPrice
+  form.isRecommend = (result as any).detail.isRecommend
+  form.isColumn = (result as any).detail.isColumn
+  form.desc = (result as any).detail.desc
+  form.detail = (result as any).detail.detail
+}
+
+const handleClick = async () => {
+  await CreateCourseForm.value?.validate((valid) => {
+    if (valid) {
+      if (props.useType === 1) {
+        // 更新
+        handleUpdateCourse()
+      } else {
+        // 创建
+        handleCreateCourse()
+      }
+    }
+  })
+}
+
+const handleCreateCourse = async () => {
+  try {
+    await httpPost('/api/course/createCourse', form)
+    ElMessage.success("创建成功")
+  } catch (err) {
+    ElMessage.error(err as any)
+    return
+  }
+}
+
+// 更新
+const handleUpdateCourse = async () => {
+  let result = null
+  try {
+    result = await httpPost("/api/course/updateCourse")
+  } catch(err) {
+    ElMessage.error(err as any)
+    return
+  }
+  form.courseId = (result as any).detail.id
+}
+
+if (props.useType === 1) {
+  getUpdateCourseDetail()
+}
+</script>
--- pages/Manage/index.vue
+++ pages/Manage/index.vue
@@ -1,3 +1,120 @@
 <template>
-  <h1>管理</h1>
+  <el-form inline>
+    <el-form-item label="课程标题">
+      <el-input v-model="form.title" placeholder="请输入课程标题" clearable />
+    </el-form-item>
+    <el-form-item label="课程现价">
+      <el-input v-model="form.price" placeholder="请输入课程标题" clearable />
+    </el-form-item>
+    <el-form-item label="课程原价">
+      <el-input v-model="form.tPrice" placeholder="请输入课程标题" clearable />
+    </el-form-item>
+    <el-form-item label="是否推荐">
+      <el-select v-model="form.isRecommend" placeholder="请选择是否推荐" clearable>
+        <el-option label="否" :value="0" />
+        <el-option label="是" :value="1" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="是否在栏目">
+      <el-select v-model="form.isColumn" placeholder="请选择是否在栏目" clearable>
+        <el-option label="否" :value="0" />
+        <el-option label="是" :value="1" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="创建时间">
+      <el-date-picker
+        v-model="form.createdAt"
+        type="datetimerange"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        :default-time="defaultTime"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary">搜索</el-button>
+      <el-button type="primary" @click="handleCreateCourse">新建课程</el-button>
+    </el-form-item>
+  </el-form>
+
+  <el-table :data="tableData.list" border>
+    <el-table-column prop="id" label="课程ID" />
+    <el-table-column prop="title" label="课程标题" />
+    <el-table-column prop="price" label="课程现价" />
+    <el-table-column prop="tPrice" label="课程原价" />
+    <el-table-column label="是否推荐">
+      <template #default="{ row }">
+        {{ row.isRecommend ? "是" : "否" }}
+      </template>
+    </el-table-column>
+    <el-table-column label="是否在栏目">
+      <template #default="{ row }">
+        {{ row.isColumn ? "是" : "否" }}
+      </template>
+    </el-table-column>
+    <el-table-column prop="createdAt" label="创建时间" />
+  </el-table>
+
+  <el-pagination
+    v-model:current-page="form.pageNo"
+    v-model:page-size="form.pageSize"
+    :page-sizes="[2, 5, 10, 20]"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="tableData.total"
+    @size-change="handleSearch"
+    @current-change="handleGetPageList" />
+
+  <CourseUpdateCourse v-if="courseForm.visible" />
 </template>
+
+<script lang="ts" setup>
+
+const defaultTime: [Date, Date] = [
+  new Date(new Date().getFullYear(), 1, 1, 0, 0, 0),
+  new Date(new Date().getFullYear(), 2, 1, 23, 59, 59),
+]
+const form = reactive({
+  title: "",
+  price: "",
+  tPrice: "",
+  isRecommend: "",
+  isColumn: "",
+  createdAt: "",
+  pageSize: 2,
+  pageNo: 1
+})
+
+const tableData = reactive({
+  list: [],
+  total: 0
+})
+
+const handleSearch = () => {
+  form.pageNo = 1
+  handleGetPageList()
+}
+
+const handleGetPageList = async () => {
+  let result = null
+  try {
+    result = await httpGet('/api/course/getCourseList', {
+      params: form
+    })
+  } catch (err) {
+    ElMessage.error(err as any)
+    return
+  }
+  tableData.list = result.list
+}
+
+handleGetPageList()
+
+
+
+// 新建课程
+const courseForm = reactive({
+  visible: false
+})
+const handleCreateCourse = () => {
+  courseForm.visible = true
+}
+</script>
